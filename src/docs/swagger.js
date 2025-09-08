8913006b49f411ce08d1e0fe322a2a723d400035
Gonzalo Lara <gonzaexelara@gmail.com>
2025-09-08 16:53:24 -0300

    cambios entrega final

--- src/docs/swagger.js
+++ src/docs/swagger.js
@@ -6,62 +6,10 @@ const options = {
     info: {
       title: 'API - Proyecto Adopciones',
       version: '1.0.0',
-      description: 'Documentacion Users',
+      description: 'Documentación de la API',
     },
-    servers: [
-      { url: 'http://localhost:4000', description: 'Local' }
-    ],
-    components: {
-      securitySchemes: {
-        cookieAuth: {
-          type: 'apiKey',
-          in: 'cookie',
-          name: 'coderCookie'
-        }
-      },
-      schemas: {
-        User: {
-          type: 'object',
-          properties: {
-            _id: { type: 'string' },
-            first_name: { type: 'string' },
-            last_name: { type: 'string' },
-            email: { type: 'string' },
-            role: { type: 'string', enum: ['user','admin'] },
-            pets: {
-              type: 'array',
-              items: {
-                type: 'object',
-                properties: {
-                  _id: { type: 'string' }
-                }
-              }
-            }
-          }
-        },
-        CreateUserInput: {
-          type: 'object',
-          required: ['first_name','last_name','email','password'],
-          properties: {
-            first_name: { type: 'string' },
-            last_name: { type: 'string' },
-            email: { type: 'string', format: 'email' },
-            password: { type: 'string', format: 'password' },
-            role: { type: 'string', enum: ['user','admin'] }
-          }
-        },
-        UpdateUserInput: {
-          type: 'object',
-          properties: {
-            first_name: { type: 'string' },
-            last_name: { type: 'string' },
-            role: { type: 'string', enum: ['user','admin'] }
-          }
-        }
-      }
-    }
   },
-  apis: ['src/routes/users.router.js'],
+  apis: ['src/docs/**/*.yaml'],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
